92a66c19591091a2963d6f90fba5d89a2763f84a
Jong012 <Jong012>
2022-12-19 18:09:19 +0900

    feat(tests): test init

--- backend/math_calc/tests.py
+++ backend/math_calc/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+# from django.test import TestCase
+import pytest
+
 
 # Create your tests here.
+
+
+# def test_add():
+#     assert 1 == 0
+
+
+@pytest.mark.urls('math_calc.urls')
+def test_something(client):
+    print('123123123132')
+    print(client.get('/math-calc/').content)
+    assert b'Success!' in client.get('/math-calc/').content
